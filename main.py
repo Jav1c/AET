1bb711e899eea8b3d5c568ec0f28c4e68ad796d0
Jav1c <101632816+Jav1c@users.noreply.github.com>
2023-12-30 18:13:58 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,17 @@
 from kivy.app import App
-from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
 from kivy.config import Config
-Config.set('graphics', 'Resizable','0')
+
+Config.set('graphics', 'Resizable', '0')
 Config.set('graphics', 'width', '412')
 Config.set('graphics', 'height', '732')
 
-class HomePage(Screen):
-    pass
-    
-class Q1(Screen):
-    pass
 
-class Q2(Screen):
+class Prototype(BoxLayout):
     pass
-
-class Q3(Screen):
-    pass
-
-class FinalPage(Screen):
-    pass
-
-class ScreenManagement(ScreenManager):
-    pass
-
-file=Builder.load_file('kivy.kv')
 
 class QuizApp(App):
-    def build(self):
-        return file
+    pass
 
 QuizApp().run()
