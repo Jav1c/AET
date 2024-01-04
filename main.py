ead6b7290ace4f243d8953b9149a5e75d8b8d067
Jav1c <101632816+Jav1c@users.noreply.github.com>
2024-01-04 16:08:41 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.config import Config
 
@@ -8,11 +10,44 @@ Config.set('graphics', 'Resizable', '0')
 Config.set('graphics', 'width', '412')
 Config.set('graphics', 'height', '732')
 
-class Prototype(FloatLayout):
+
+class Q1(Screen):
     pass
+
+class Q2(Screen):
+    pass
+
+class Q3(Screen):
+    pass
+
+class Q4(Screen):
+    pass
+
+class Q5(Screen):
+    pass
+
+class Q6(Screen):
+    pass
+
+class Q7(Screen):
+    pass
+
+class Q8(Screen):
+    pass
+
+class Q9(Screen):
+    pass
+
+class Q10(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file('Quiz.kv')
 
 class QuizApp(App):
     def build(self):
-        return Prototype()
+        return kv
 
 QuizApp().run()
