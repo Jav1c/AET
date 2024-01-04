dd655be74ac0265323ea92262e8d7ad14f6a9862
Jav1c <101632816+Jav1c@users.noreply.github.com>
2024-01-04 11:21:04 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.config import Config
 
@@ -7,11 +8,11 @@ Config.set('graphics', 'Resizable', '0')
 Config.set('graphics', 'width', '412')
 Config.set('graphics', 'height', '732')
 
-
-class Prototype(BoxLayout):
+class Prototype(FloatLayout):
     pass
 
 class QuizApp(App):
-    pass
+    def build(self):
+        return Prototype()
 
 QuizApp().run()
